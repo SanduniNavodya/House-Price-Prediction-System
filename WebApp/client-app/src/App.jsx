6a2846c6230ed88521db6d31f79fc8bc28a47e24
Ruchira Dasanayake <drl.dasanayake.official@gmail.com>
2024-09-24 19:19:43 +0530

    added bootstrap

--- WebApp/client-app/src/App.jsx
+++ WebApp/client-app/src/App.jsx
@@ -19,33 +19,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header text-center p-3 bg-primary text-white">
+        House Price Prediction System
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <div>
-          {array.map((user, index) => {
-            return (
-              <div key={index}>
-                <span>{user}</span>
-                <br></br>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
